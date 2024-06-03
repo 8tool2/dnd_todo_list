b99b176b3e3a57ec025ae09c3f117a22f6ea387e
asuru56 <56asuru@gmail.com>
2024-06-03 16:49:38 +0530

    Added New UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ToDoList from './ToDoList';
+import App from'./App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ToDoList />
+    <App />
   </React.StrictMode>
 );
